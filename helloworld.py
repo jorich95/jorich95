5b8a93606683443113ce2ca1b0217622d15f1494
jorich95 <81038726+jorich95@users.noreply.github.com>
2021-03-21 22:45:51 +0000

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+print("How are you?")
+
+print("---------------------------")
+
+if 5>2:
+	print("Five is greater than 2")
+if 10>2:
+	print("Ten is greater than two!")
+
+print("---------------------------")
+
+first_name = "Troy"
+last_name = "Baker"
+age = 20
+
+print(first_name)
+print(last_name)
+print(age)
+
+#print("---------------------------")
