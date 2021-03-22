638b80805759651a7bf0b1350225a5a84a6f840b
jorich95 <81038726+jorich95@users.noreply.github.com>
2021-03-22 23:33:12 +0000

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -18,4 +18,61 @@ print(first_name)
 print(last_name)
 print(age)
 
-#print("---------------------------")
+"""
+Print("Testing")
+Print("Testing")
+Print(Testing")
+"""
+print("---------------------------")
+
+print("I will now rename the first_name variable")
+
+first_name = "Simon"
+print(first_name)
+
+print("I will now rename the first_name variable again")
+first_name = "Tony"
+print(first_name)
+print("I will now change the first_name variable type to an integer")
+first_name = 9865.456
+print(first_name)
+
+print("---------------------------") #print("The End!")
+
+print("VARIABLES")
+#Casting
+x = int(3)
+y = str(3)
+z = float(3)
+print(x) 
+print(y)
+print(z)
+
+print("---------------------------")
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+
+print("---------------------------")
+
+#Quotes ('') or ("")
+name_1 = 'James'
+name_2 = "James"
+print(type(name_1))
+print(type(name_2))
+print(name_1)
+print(name_2)
+
+print("---------------------------")
+#variable names are case sensitive
+NAME_1 = "John"
+NAME_2 = 'Marcus'
+
+print(name_1)
+print(NAME_1)
+print(name_2)
+print(NAME_2)
+print(type(NAME_2))
+
+
